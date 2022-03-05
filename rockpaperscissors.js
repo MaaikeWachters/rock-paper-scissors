73b4ae06f7f2a98842a5b9eda9969f9eb093d870
MaaikeWachters <73661817+MaaikeWachters@users.noreply.github.com>
2022-03-05 09:45:15 +0100

    Added display of computer choice & display of player choice

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -6,13 +6,25 @@
 // reset & play again
 
 let computerPlay = document.getElementById("computer-play");
-
-let playerInput = "rock";
+let computerResult = document.getElementById("computer-result");
+let playerPlay = document.getElementById("player-play");
 
 let computerOptions = ["Rock", "Paper", "Scissors"];
 
 computerPlay.addEventListener("click", function () {
+  let getRandomPlay = "";
   let randomIndex = Math.floor(Math.random() * computerOptions.length);
-  return computerOptions[randomIndex];
+  getRandomPlay = computerOptions[randomIndex];
+  computerResult.innerHTML = getRandomPlay;
 });
+
+playerPlay.addEventListener("click", function () {
+  let playerChoice = document.getElementsByName("player");
+  for (let i = 0; i < playerChoice.length; i++) {
+    if (playerChoice[i].checked)
+      document.getElementById("players-choice").innerHTML =
+        playerChoice[i].value;
+  }
+});
+
 
