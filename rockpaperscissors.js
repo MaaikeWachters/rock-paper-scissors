d84c07abdbecf7711a5ace78962984f4bda80dd2
MaaikeWachters <73661817+MaaikeWachters@users.noreply.github.com>
2022-03-05 11:24:48 +0100

    Added 1 second delay for winner declaration, to add suspense ;)

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,10 +1,3 @@
-// TO DO:
-// computer plays: random return of Rock, Paper Scissors
-// player plays: choose Rock, Paper Scissors
-// compare computer & player input: rock beats scissors, paper beats rock, scissors beats paper
-// show winner
-// reset & play again
-
 let computerPlayBtn = document.getElementById("computer-play");
 let computerResult = document.getElementById("computer-result");
 let computerChoice = "";
@@ -22,11 +15,11 @@ function getComputerPlay() {
 }
 
 function getPlayerPlay() {
-  let ele = document.getElementsByName("player");
-  for (let i = 0; i < ele.length; i++) {
-    if (ele[i].checked) {
-      document.getElementById("players-choice").innerHTML = ele[i].value;
-      playerChoice = ele[i].value;
+  let player = document.getElementsByName("player");
+  for (let i = 0; i < player.length; i++) {
+    if (player[i].checked) {
+      document.getElementById("players-choice").innerHTML = player[i].value;
+      playerChoice = player[i].value;
     }
   }
 }
@@ -41,19 +34,21 @@ computerPlayBtn.addEventListener("click", function () {
 });
 
 function displayWinner() {
-  if (computerChoice === playerChoice) {
-    winnerDeclaration.innerHTML = "It's a tie";
-  } else if (computerChoice === "Rock" && playerChoice === "Scissors") {
-    winnerDeclaration.innerHTML = "Computer";
-  } else if (computerChoice === "Paper" && playerChoice === "Scissors") {
-    winnerDeclaration.innerHTML = "YOU";
-  } else if (computerChoice === "Rock" && playerChoice === "Paper") {
-    winnerDeclaration.innerHTML = "YOU";
-  } else if (computerChoice === "Paper" && playerChoice === "Rock") {
-    winnerDeclaration.innerHTML = "Computer";
-  } else if (computerChoice === "Scissors" && playerChoice === "Paper") {
-    winnerDeclaration.innerHTML = "Computer";
-  } else {
-    winnerDeclaration.innerHTML = "YOU";
-  }
+  setTimeout(function () {
+    if (computerChoice === playerChoice) {
+      winnerDeclaration.innerHTML = "It's a tie";
+    } else if (computerChoice === "Rock" && playerChoice === "Scissors") {
+      winnerDeclaration.innerHTML = "Computer";
+    } else if (computerChoice === "Paper" && playerChoice === "Scissors") {
+      winnerDeclaration.innerHTML = "YOU";
+    } else if (computerChoice === "Rock" && playerChoice === "Paper") {
+      winnerDeclaration.innerHTML = "YOU";
+    } else if (computerChoice === "Paper" && playerChoice === "Rock") {
+      winnerDeclaration.innerHTML = "Computer";
+    } else if (computerChoice === "Scissors" && playerChoice === "Paper") {
+      winnerDeclaration.innerHTML = "Computer";
+    } else {
+      winnerDeclaration.innerHTML = "YOU";
+    }
+  }, 1000);
 }
