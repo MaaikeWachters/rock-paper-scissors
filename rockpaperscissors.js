482ea3d5045f587961afac4b4b1ed445077a932d
MaaikeWachters <73661817+MaaikeWachters@users.noreply.github.com>
2022-03-05 11:06:32 +0100

    Added gameplay & reset button functionality

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -5,26 +5,55 @@
 // show winner
 // reset & play again
 
-let computerPlay = document.getElementById("computer-play");
+let computerPlayBtn = document.getElementById("computer-play");
 let computerResult = document.getElementById("computer-result");
-let playerPlay = document.getElementById("player-play");
-
+let computerChoice = "";
 let computerOptions = ["Rock", "Paper", "Scissors"];
 
-computerPlay.addEventListener("click", function () {
-  let getRandomPlay = "";
+let playerPlayBtn = document.getElementById("player-play");
+let playerChoice = "";
+
+let winnerDeclaration = document.getElementById("winner");
+
+function getComputerPlay() {
   let randomIndex = Math.floor(Math.random() * computerOptions.length);
-  getRandomPlay = computerOptions[randomIndex];
-  computerResult.innerHTML = getRandomPlay;
-});
+  computerChoice = computerOptions[randomIndex];
+  computerResult.innerHTML = computerChoice;
+}
 
-playerPlay.addEventListener("click", function () {
-  let playerChoice = document.getElementsByName("player");
-  for (let i = 0; i < playerChoice.length; i++) {
-    if (playerChoice[i].checked)
-      document.getElementById("players-choice").innerHTML =
-        playerChoice[i].value;
+function getPlayerPlay() {
+  let ele = document.getElementsByName("player");
+  for (let i = 0; i < ele.length; i++) {
+    if (ele[i].checked) {
+      document.getElementById("players-choice").innerHTML = ele[i].value;
+      playerChoice = ele[i].value;
+    }
   }
+}
+
+playerPlayBtn.addEventListener("click", function () {
+  getPlayerPlay();
 });
 
+computerPlayBtn.addEventListener("click", function () {
+  getComputerPlay();
+  displayWinner();
+});
 
+function displayWinner() {
+  if (computerChoice === playerChoice) {
+    winnerDeclaration.innerHTML = "It's a tie";
+  } else if (computerChoice === "Rock" && playerChoice === "Scissors") {
+    winnerDeclaration.innerHTML = "Computer";
+  } else if (computerChoice === "Paper" && playerChoice === "Scissors") {
+    winnerDeclaration.innerHTML = "YOU";
+  } else if (computerChoice === "Rock" && playerChoice === "Paper") {
+    winnerDeclaration.innerHTML = "YOU";
+  } else if (computerChoice === "Paper" && playerChoice === "Rock") {
+    winnerDeclaration.innerHTML = "Computer";
+  } else if (computerChoice === "Scissors" && playerChoice === "Paper") {
+    winnerDeclaration.innerHTML = "Computer";
+  } else {
+    winnerDeclaration.innerHTML = "YOU";
+  }
+}
